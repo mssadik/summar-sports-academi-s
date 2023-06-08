7c9baf5a34ace29181ef60a790613263616238c0
Sadik <anarul6777@gmail.com>
2023-06-08 13:37:32 +0600

    instroctor data loaded

--- index.js
+++ index.js
@@ -1,11 +1,64 @@
 const  express = require('express');
 const app = express();
 const cors = require('cors');
+require('dotenv').config();
 const port = process.env.PORT || 5000;
 
 //middleware
 app.use(cors());
 app.use(express.json());
+
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.mhsnbkd.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const classesCollection = client.db("sports").collection("classesCollection");
+    const instructorsCollection = client.db("sports").collection("instructorsCollection");
+    const reviewCollection = client.db("sports").collection("reviewCollection");
+
+    // classess collections
+    app.get('/classes', async (req, res) => {
+        const result = await classesCollection.find().toArray();
+        res.send(result);
+      })
+
+    // instroctor collections
+    app.get('/instructors', async(req, res) =>{
+        const result = await instructorsCollection.find().toArray();
+        res.send(result);
+    })
+
+    // review collection
+    app.get('/review', async(req, res) =>{
+        const result = await reviewCollection.find().toArray();
+        res.send(result);
+    })
+      
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
 
 
 app.get('/', (req, res) =>{
