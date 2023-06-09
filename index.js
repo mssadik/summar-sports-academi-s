cefdb8bc75dabe25f4c413309601ea882a5cbf7a
Sadik <anarul6777@gmail.com>
2023-06-09 15:23:00 +0600

    delete api is done

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const  express = require('express');
+const express = require('express');
 const app = express();
 const cors = require('cors');
 require('dotenv').config();
@@ -10,7 +10,7 @@ app.use(express.json());
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.mhsnbkd.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -28,27 +28,61 @@ async function run() {
     await client.connect();
 
     const classesCollection = client.db("sports").collection("classesCollection");
+    const classessCartCollection = client.db("sports").collection("classessCartCollection");
     const instructorsCollection = client.db("sports").collection("instructorsCollection");
     const reviewCollection = client.db("sports").collection("reviewCollection");
 
     // classess collections here
     app.get('/classes', async (req, res) => {
-        const result = await classesCollection.find().toArray();
-        res.send(result);
-      })
+      const result = await classesCollection.find().toArray();
+      res.send(result);
+    })
+
+    // carts collections here
+    app.get('/carts', async (req, res) => {
+      const email = req.query.email;
+
+      // if (!email) {
+      //  return res.send([]);
+      // }
+      const query = { userEmail: email };
+      const result = await classessCartCollection.find(query).toArray();
+      res.send(result);
+    });
+
+    app.delete('/carts/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await classessCartCollection.deleteOne(query);
+      res.send(result);
+    })
+
+
+    // app.get('/carts', async (req, res) => {
+    //   const result = await classessCartCollection.find().toArray();
+    //   res.send(result);
+    // });
+   
+
+    app.post('/carts', async (req, res) => {
+      const item = req.body;
+      console.log(item);
+      const result = await classessCartCollection.insertOne(item);
+      res.send(result);
+    })
 
     // instroctor collections here
-    app.get('/instructors', async(req, res) =>{
-        const result = await instructorsCollection.find().toArray();
-        res.send(result);
+    app.get('/instructors', async (req, res) => {
+      const result = await instructorsCollection.find().toArray();
+      res.send(result);
     })
 
     // review collection here
-    app.get('/review', async(req, res) =>{
-        const result = await reviewCollection.find().toArray();
-        res.send(result);
+    app.get('/review', async (req, res) => {
+      const result = await reviewCollection.find().toArray();
+      res.send(result);
     })
-      
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
@@ -61,10 +95,10 @@ run().catch(console.dir);
 
 
 
-app.get('/', (req, res) =>{
-    res.send("too much summar")
+app.get('/', (req, res) => {
+  res.send("too much summar")
 })
 
-app.listen(port, () =>{
-    console.log(`summar is running on prot: ${port}`)
+app.listen(port, () => {
+  console.log(`summar is running on prot: ${port}`)
 })
